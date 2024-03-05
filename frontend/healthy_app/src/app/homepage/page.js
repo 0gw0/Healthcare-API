ed56c9cb863c53ce69bebdc0fca7fce0931267a9
0Ogw <glen.wang2709@gmail.com>
2024-03-06 00:36:23 +0800

    Homepage
    Homepage with navbar and hero section

--- frontend/healthy_app/src/app/homepage/page.js
+++ frontend/healthy_app/src/app/homepage/page.js
@@ -1,3 +1,38 @@
+import Navbar from '@/components/Navbar';
+import Container from '@/components/container';
+import Hero from '@/components/hero';
+
 export default function Page() {
-	return <h1>Hello, Next.js!</h1>;
+	return (
+		<>
+			<Navbar />
+			<Hero />
+			<div className="flex flex-row">
+				<div className="basis-8/12">
+					<Container>
+						<h1 className="text-2xl font-bold leading-snug tracking-tight text-gray-800 lg:text-2xl lg:leading-tight xl:text-4xl xl:leading-tight dark:text-white">
+							Specialty
+						</h1>
+					</Container>
+					<div className="flex flex-row">
+						<Container className="basis-1/2">
+							<h1 className="text-2xl font-bold leading-snug tracking-tight text-gray-800 lg:text-2xl lg:leading-tight xl:text-4xl xl:leading-tight dark:text-white">
+								Prescription Status
+							</h1>
+						</Container>
+						<Container className="basis-1/2">
+							<h1 className="text-2xl font-bold leading-snug tracking-tight text-gray-800 lg:text-2xl lg:leading-tight xl:text-4xl xl:leading-tight dark:text-white">
+								History
+							</h1>
+						</Container>
+					</div>
+				</div>
+				<Container className="basis-4/12">
+					<h1 className="text-2xl font-bold leading-snug tracking-tight text-gray-800 lg:text-2xl lg:leading-tight xl:text-4xl xl:leading-tight dark:text-white">
+						Upcoming Appointments
+					</h1>
+				</Container>
+			</div>
+		</>
+	);
 }
---
+++ frontend/healthy_app/src/components/hero.js
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import heroImg from '../../public/hero.svg';
+import Container from './container';
+
+const Hero = () => {
+	return (
+		<>
+			<Container>
+				<div className="flex items-center justify-center gap-6 lg:gap-28 xl:gap-36">
+					<div className="max-w-2xl">
+						<h1 className="text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:text-3xl lg:leading-tight xl:text-5xl xl:leading-tight dark:text-white">
+							Homepage
+						</h1>
+						<p className="py-5 text-lg leading-normal text-gray-500 lg:text-lg xl:text-xl dark:text-gray-300">
+							Book and manage your consultations below
+						</p>
+					</div>
+					<div className="flex items-center justify-center">
+						<div className="w-64">
+							<Image
+								src={heroImg}
+								alt="Hero Illustration"
+								loading="eager"
+							/>
+						</div>
+					</div>
+				</div>
+			</Container>
+			<hr className="w-9/12 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700" />
+		</>
+	);
+};
+
+export default Hero;
