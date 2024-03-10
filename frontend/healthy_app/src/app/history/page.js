192dff51914304f8137f703670ed8cd089e56971
hyinki <113676721+hyinki@users.noreply.github.com>
2024-03-10 21:31:13 +0800

    history

---
+++ frontend/healthy_app/src/app/history/page.js
@@ -0,0 +1,48 @@
+"use client"; // This is a client component
+
+import Navbar from "@/components/Navbar";
+import Container from "@/components/container";
+import Hero from "@/components/hero";
+import Footer from "@/components/footer";
+import HistoryLog from "@/components/HistoryLog";
+
+
+
+import React, { useState } from "react";
+
+export default function Page() {
+    const medicationLogData = [
+        { id: 1, date: '2024-03-01', time: '01:00 PM', doctorname: "Dr Peter Lam", medicinePrescribed: 'Paracetamol', mcissued:"Yes", medicineDelivered: 'Yes' },
+        { id: 2, date: '2024-03-09', time: '05:00 PM', doctorname: "Dr Alice Tan", medicinePrescribed: 'Paracetamol', mcissued:"Yes", medicineDelivered: 'No' }
+      ];
+      
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      {/* User Homepage items */}
+      <Container>
+        <div className="flex flex-row">
+          <div className="basis-5/12">
+            <Container>
+              <h1 className="ml-4 text-2xl font-bold leading-snug tracking-tight text-gray-800 lg:text-2xl lg:leading-tight xl:text-4xl xl:leading-tight dark:text-white">
+                Past Teleconsultation Details
+              </h1>
+              
+            </Container>
+
+            
+          </div>
+          
+        </div>
+
+        <HistoryLog data={medicationLogData}></HistoryLog>
+       
+      </Container>
+
+      <div className="container mx-auto p-4 flex justify-center"></div>
+
+      <Footer />
+    </>
+  );
+}
---
+++ frontend/healthy_app/src/components/HistoryLog.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+const HistoryLog = ({ data }) => {
+    
+  return (
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="overflow-hidden border border-gray-200 rounded-md">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Date
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font- medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Time
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Doctor Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Medicine Prescribed
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    MC issued
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Medicine Delivered
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {data.map((item) => (
+                  <tr key={item.id}>
+                    <td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium text-gray-900">
+                      {item.date}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium text-gray-900">
+                      {item.time}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium text-gray-900">
+                      {item.doctorname}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium text-gray-900">
+                      {item.medicinePrescribed}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium text-gray-900">
+                      {item.mcissued}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium text-gray-900">
+                      {item.medicineDelivered}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HistoryLog;
