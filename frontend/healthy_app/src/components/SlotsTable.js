acf188233ca7a632d6576f0f32d5bccb923e822b
0Ogw <glen.wang2709@gmail.com>
2024-03-12 09:07:02 +0800

    Visual update and code refactoring

--- frontend/healthy_app/src/components/SlotsTable.js
+++ frontend/healthy_app/src/components/SlotsTable.js
@@ -9,29 +9,29 @@ const SlotsTable = ({ data }) => {
 					<div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
 						<div className="overflow-hidden border border-gray-200 rounded-md">
 							<table className="min-w-full divide-y divide-gray-200">
-								<thead>
+								<thead className="bg-blue-950 text-white">
 									<tr>
 										<th
 											scope="col"
-											className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+											className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
 										>
 											Date
 										</th>
 										<th
 											scope="col"
-											className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+											className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
 										>
 											Time
 										</th>
 										<th
 											scope="col"
-											className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+											className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
 										>
 											Patient Name
 										</th>
 										<th
 											scope="col"
-											className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+											className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
 										>
 											Appointment Type
 										</th>
@@ -39,17 +39,20 @@ const SlotsTable = ({ data }) => {
 								</thead>
 								<tbody className="bg-white divide-y divide-gray-200">
 									{data.map((item) => (
-										<tr key={item.id}>
-											<td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium text-gray-900">
+										<tr
+											key={item.id}
+											className="text-gray-900"
+										>
+											<td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium ">
 												{item.date}
 											</td>
-											<td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium text-gray-900">
+											<td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">
 												{item.time}
 											</td>
-											<td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium text-gray-900">
+											<td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">
 												{item.ptName}
 											</td>
-											<td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium text-gray-900">
+											<td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">
 												{item.appType}
 											</td>
 										</tr>
