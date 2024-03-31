37fad7387026eb64c71af319f0f35bffa9b76ead
0Ogw <glen.wang2709@gmail.com>
2024-03-31 10:05:08 +0800

    Docker compose frontend
    removed <suspenseboundary> req in next.config to remove npm run build error

--- frontend/healthy_app/next.config.mjs
+++ frontend/healthy_app/next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: "/callback",
-                destination: "/",
-                permanent: true,
-            },
-        ];
-    },
+	async redirects() {
+		return [
+			{
+				source: '/callback',
+				destination: '/',
+				permanent: true,
+			},
+		];
+	},
+	experimental: {
+		missingSuspenseWithCSRBailout: false,
+	},
 };
 
 export default nextConfig;
